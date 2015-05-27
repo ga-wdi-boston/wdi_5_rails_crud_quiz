c038e0e8dfbb7d067d3a4195886c453574df9806
aaronw3373 <aaronw3373@gmail.com>
2015-05-27 10:20:17 -0400

    fixed a multitude of spelling mistakes

--- scripts/save.rb
+++ scripts/save.rb
@@ -7,9 +7,9 @@ House.create(name:'House Bolton',mott:'Our blades are sharp.',sigil:'http://i.im
 House.create(name:'House Lannister',mott:'Hear me roar!',sigil:'http://i.imgur.com/gOwmzoU.jpg');
 House.create(name:'House Stark',mott:'Winter is coming.',sigil:'http://i.imgur.com/zpVjXsa.jpg');
 
-People.create(name:'Daenerys Targaryen',title:'Queen of Meereen and Mother of Dragons',quote:'I will take what is mine with blood and fire.',image:'http://i.imgur.com/DjHJq4E.jpg',age:18);
-People.create(name:'Tyrion Lannister',title:'Hand of the King and Master of Coin',quote:'A Lannister always pays his debts.',image:'http://i.imgur.com/rNHNWca.jpg',age:35);
-People.create(name:'Arya Stark',title:'Princess of Winterfell',quote:'...anyone can be killed.',image:'http://i.imgur.com/Csq9exg.jpg',age:14);
+Person.create(name:'Daenerys Targaryen',title:'Queen of Meereen and Mother of Dragons',quote:'I will take what is mine with blood and fire.',imiage:'http://i.imgur.com/DjHJq4E.jpg',age:18);
+Person.create(name:'Tyrion Lannister',title:'Hand of the King and Master of Coin',quote:'A Lannister always pays his debts.',imiage:'http://i.imgur.com/rNHNWca.jpg',age:35);
+Person.create(name:'Arya Stark',title:'Princess of Winterfell',quote:'...anyone can be killed.',imiage:'http://i.imgur.com/Csq9exg.jpg',age:14);
 
 
 
