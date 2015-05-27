01e285c934d2522a020aa09b935b0cb5869cbead
Kinjalc <kinjal.chauhan@gmail.com>
2015-05-27 10:07:28 -0400

    commit

---
+++ scripts/save.rb
@@ -0,0 +1,5 @@
+House.create(name:'Baratheon', motto: 'Ours is the fury', sigil: 'http://i.imgur.com/iJtBKIR.png');
+House.create(name:'Bolton', motto: 'Our blades are sharp.', sigil: 'http://i.imgur.com/uPoc9Jh.jpg');
+House.create(name:'Lannister', motto: 'Hear me roar!', sigil: 'http://i.imgur.com/gOwmzoU.jpg');
+People.create(name:'Daenerys Targaryen', title: 'Queen of Meereen and Mother of Dragons', quote:'I will take what is mine with blood and fire', image:'http://i.imgur.com/DjHJq4E.jpg', age:18);
+People.create(name:'Jon Snow', title: 'Lord Commander of the Night''s Watch', quote:'Night gathers, and now my watch begins.', image:'http://i.imgur.com/WadV6VI.jpg', age:19);
