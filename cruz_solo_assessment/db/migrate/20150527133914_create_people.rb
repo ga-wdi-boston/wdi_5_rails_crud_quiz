e61d570ef60e7765dbfdabf317a4e794ba0b558e
breduffy <breanne.duffy@gmail.com>
2015-05-27 10:06:17 -0400

    quiz.

---
+++ cruz_solo_assessment/db/migrate/20150527133914_create_people.rb
@@ -0,0 +1,13 @@
+class CreatePeople < ActiveRecord::Migration
+  def change
+    create_table :people do |t|
+      t.string :name
+      t.string :title
+      t.string :quote
+      t.string :image
+      t.integer :age
+
+      t.timestamps null: false
+    end
+  end
+end
