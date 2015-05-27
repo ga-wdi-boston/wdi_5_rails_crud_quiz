c9a4caec9fbb36df654cc0be395ccb22090f2531
courtneysadusky <courtneysadusky@gmail.com>
2015-05-27 10:07:24 -0400

    changes to quiz

---
+++ cruz_solo_assessment/db/migrate/20150527133939_create_people.rb
@@ -0,0 +1,4 @@
+class CreatePeople < ActiveRecord::Migration
+  def change
+   person.age > 20
+end
---
+++ cruz_solo_assessment/scripts/add_people.rb
@@ -0,0 +1,4 @@
+Person.Create name:'Daenerys Targaryen' titile:'Queen of Meereen and Mother of Dragons' quote:'I will take what is mine with blood and fire.' image:'http://i.imgur.com/DjHJq4E.jpg' age:18
+
+Person.Create name:'Jon Snow' title:'Lord Commander of the Nights Watch'quote:'Night gathers, and now my watch begins.' image:'http://i.imgur.com/WadV6VI.jpg' age:19
+Person.Create name:'Tyrion Lannister' title:'Hand of the King and Master of Coin' motto:'A Lannister always pays his debts.' image:'http://i.imgur.com/rNHNWca.jpg' age:35
