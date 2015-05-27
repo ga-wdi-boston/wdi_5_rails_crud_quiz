6ec492758aaab268efb47d67ac0a2e7f1a9972c7
alik864 <alikarukas@gmail.com>
2015-05-27 09:59:28 -0400

    quiz

---
+++ scripts/delete.rb
@@ -0,0 +1,6 @@
+
+// Question 5. Kill off, er, I mean delete one of the people. Look them up by their title.
+  + Save your code in `scripts/delete.rb`
+
+DELETE FROM People
+WHERE id = 2;
---
+++ scripts/find.rb
@@ -0,0 +1,4 @@
+
+Question 3. Use ActiveRecord to retrieve all of the people greater than a certain age *or* having a specific title.
+
+select * FROM people WHERE age > 35 or title = 'Decesased';
