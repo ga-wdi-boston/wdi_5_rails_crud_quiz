3e085e4ba43ae9b391b2b74c7095eb3400dfec11
aaronw3373 <aaronw3373@gmail.com>
2015-05-27 10:05:30 -0400

    some changes to save that fixed some syntax errors

--- scripts/save.rb
+++ scripts/save.rb
@@ -2,14 +2,14 @@ Person.destroy_all
 House.destroy_all
 
 
-House.create(name:'House Baratheon',motto:'Ours is the fury.',sigil:'http://i.imgur.com/iJtBKIR.png');
-House.create(name:'House Bolton',motto:'Our blades are sharp.',sigil:'http://i.imgur.com/uPoc9Jh.jpg');
-House.create(name:'House Lannister',motto:'Hear me roar!',sigil:'http://i.imgur.com/gOwmzoU.jpg');
-House.create(name:'House Stark',motto:'Winter is coming.',sigil:'http://i.imgur.com/zpVjXsa.jpg');
+House.create(name:'House Baratheon',mott:'Ours is the fury.',sigil:'http://i.imgur.com/iJtBKIR.png');
+House.create(name:'House Bolton',mott:'Our blades are sharp.',sigil:'http://i.imgur.com/uPoc9Jh.jpg');
+House.create(name:'House Lannister',mott:'Hear me roar!',sigil:'http://i.imgur.com/gOwmzoU.jpg');
+House.create(name:'House Stark',mott:'Winter is coming.',sigil:'http://i.imgur.com/zpVjXsa.jpg');
 
-People.crete(name:'Daenerys Targaryen',title:'Queen of Meereen and Mother of Dragons',quote:'I will take what is mine with blood and fire.',image:'http://i.imgur.com/DjHJq4E.jpg',age:18);
-People.crete(name:'Tyrion Lannister',title:'Hand of the King and Master of Coin',quote:'A Lannister always pays his debts.',image:'http://i.imgur.com/rNHNWca.jpg',age:35);
-People.crete(name:'Arya Stark',title:'Princess of Winterfell',quote:'...anyone can be killed.',image:'http://i.imgur.com/Csq9exg.jpg',age:14);
+People.create(name:'Daenerys Targaryen',title:'Queen of Meereen and Mother of Dragons',quote:'I will take what is mine with blood and fire.',image:'http://i.imgur.com/DjHJq4E.jpg',age:18);
+People.create(name:'Tyrion Lannister',title:'Hand of the King and Master of Coin',quote:'A Lannister always pays his debts.',image:'http://i.imgur.com/rNHNWca.jpg',age:35);
+People.create(name:'Arya Stark',title:'Princess of Winterfell',quote:'...anyone can be killed.',image:'http://i.imgur.com/Csq9exg.jpg',age:14);
 
 
 
